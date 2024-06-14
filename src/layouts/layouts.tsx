7a7357ea6c64c4c0a68e0be12ae68798573c5359
αѕнкαη <as6kan.developer@gmail.com>
2024-06-14 17:07:28 +0330

    feat Protected routes

--- src/layouts/layouts.tsx
+++ src/layouts/layouts.tsx
@@ -1,11 +1,34 @@
+import { useQuery } from "react-query";
+import { useNavigate } from "react-router-dom";
+
 import LeftSideBar from "../components/LeftSideBar";
 import PageContent from "../components/PageContent";
 import RightSidebar from "../components/RightSideBar";
+import supabase from "../utils/supapase";
 
 const Layouts = () => {
+  const navigate = useNavigate();
+
+  const query = useQuery({
+    queryKey: ["auth"],
+    queryFn: () => supabase.auth.getSession(),
+    select(data) {
+      if (!data.data.session) {
+        throw new Error("user in not logged in");
+      }
+    },
+  });
+
+  if (query.isLoading) {
+    return "Loading...";
+  }
+
+  if (query.error) {
+    return navigate("/login", { replace: true });
+  }
+
   return (
     <>
-      {/* Left drawer - containing page content and side bar (always open) */}
       <div className="drawer  lg:drawer-open">
         <input
           id="left-sidebar-drawer"
@@ -18,7 +41,6 @@ const Layouts = () => {
 
       <RightSidebar />
 
-      {/* Modal layout container */}
       {/* <ModalLayout /> */}
     </>
   );
--- src/main.tsx
+++ src/main.tsx
@@ -2,14 +2,19 @@ import "./assets/css/global.css";
 
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { QueryClient, QueryClientProvider } from "react-query";
 import { BrowserRouter } from "react-router-dom";
 
 import App from "./App";
 
+const queryClient = new QueryClient();
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <QueryClientProvider client={queryClient}>
+        <App />
+      </QueryClientProvider>
     </BrowserRouter>
   </React.StrictMode>,
 );
