79d715094b56e867fdab35848039dabdfb06a0eb
αѕнкαη <as6kan.developer@gmail.com>
2024-06-14 13:58:00 +0330

    feat register supabase

--- src/components/InputText.tsx
+++ src/components/InputText.tsx
@@ -1,17 +1,26 @@
 interface InputTextProps {
+  type: string;
+  value: string;
+  changeHandle: (event: React.ChangeEvent<HTMLInputElement>) => void;
+  name?: string;
   labelTitle: string;
   containerStyle?: string;
-  type?: string;
 }
 
 const InputText = (props: InputTextProps) => {
-  const { labelTitle, containerStyle, type } = props;
+  const { labelTitle, containerStyle, type, value, changeHandle, name } = props;
   return (
     <div className={`form-control w-full ${containerStyle ?? ""}`}>
       <label className="label">
         <span className="label-text text-base-content">{labelTitle}</span>
       </label>
-      <input type={type ?? "text"} className="input  input-bordered w-full " />
+      <input
+        value={value}
+        onChange={changeHandle}
+        name={name}
+        type={type}
+        className="input  input-bordered w-full "
+      />
     </div>
   );
 };
--- src/pages/Auth/Register.tsx
+++ src/pages/Auth/Register.tsx
@@ -1,11 +1,48 @@
-import { useState } from "react";
+import { useFormik } from "formik";
 import { Link } from "react-router-dom";
+import * as Yup from "yup";
 
 import InputText from "../../components/InputText";
+import supabase from "../../utils/supapase";
 import LandingIntro from "./LoginIntro";
 
+const initialValues = {
+  name: "",
+  email: "",
+  password: "",
+};
+
 const Register = () => {
-  const [loading] = useState(false);
+  const validationSchema = Yup.object({
+    name: Yup.string().required("This field is required"),
+    email: Yup.string()
+      .email("Please check Your Email")
+      .required("This field is required"),
+    password: Yup.string()
+      .min(6, "Password must be at least 6 characters")
+      .required("This field is required"),
+  });
+
+  const form = useFormik({
+    validationSchema,
+    initialValues,
+    onSubmit: async (values) => {
+      await supabase.auth.signUp({
+        options: {
+          data: {
+            name: values.name,
+          },
+        },
+        email: values.email,
+        password: values.password,
+      });
+
+      setTimeout(() => {
+        form.setSubmitting(false);
+      }, 5000);
+    },
+  });
+
   return (
     <div className="min-h-screen bg-base-200 flex items-center">
       <div className="card mx-auto w-full max-w-5xl  shadow-xl">
@@ -17,31 +54,59 @@ const Register = () => {
             <h2 className="text-2xl font-semibold mb-2 text-center">
               Register
             </h2>
-            <form>
+            <form onSubmit={form.handleSubmit}>
               <div className="mb-4">
-                <InputText containerStyle="mt-4" labelTitle="Name" />
-
-                <InputText containerStyle="mt-4" labelTitle="Email Id" />
+                <InputText
+                  value={form.values.name}
+                  changeHandle={form.handleChange}
+                  name="name"
+                  type="text"
+                  containerStyle="mt-4"
+                  labelTitle="Name"
+                />
+                <p className="mt-2 text-red-300 text-sm">
+                  {form.touched.name ? form.errors.name : null}
+                </p>
 
                 <InputText
+                  changeHandle={form.handleChange}
+                  value={form.values.email}
+                  name="email"
+                  type="email"
+                  containerStyle="mt-4"
+                  labelTitle="Email Id"
+                />
+
+                <p className="mt-2 text-red-300 text-sm">
+                  {form.touched.email ? form.errors.email : null}
+                </p>
+
+                <InputText
+                  value={form.values.password}
+                  changeHandle={form.handleChange}
+                  name="password"
                   type="password"
                   containerStyle="mt-4"
                   labelTitle="Password"
                 />
+
+                <p className="mt-2 text-red-300 text-sm">
+                  {form.touched.password ? form.errors.password : null}
+                </p>
               </div>
 
-              {/* <ErrorText styleClass="mt-8">{errorMessage}</ErrorText> */}
               <button
+                disabled={form.isSubmitting}
                 type="submit"
-                className={`btn mt-2 w-full btn-primary ${loading ? " loading" : ""}`}
+                className="btn mt-2 w-full btn-primary"
               >
-                Register
+                {form.isSubmitting ? "Loading..." : "Register"}
               </button>
 
               <div className="text-center mt-4">
                 Already have an account?{" "}
                 <Link to="/login">
-                  <span className="  inline-block  hover:text-primary hover:underline hover:cursor-pointer transition duration-200">
+                  <span className="inline-block  hover:text-primary hover:underline hover:cursor-pointer transition duration-200">
                     Login
                   </span>
                 </Link>
