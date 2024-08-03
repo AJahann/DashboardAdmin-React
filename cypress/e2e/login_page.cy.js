9463401edab61ddcef2d6fbf7f8ae6b58c2e3501
αѕнкαη <as6kan.developer@gmail.com>
2024-08-04 00:24:34 +0330

    ete test for login page //complete

--- cypress/e2e/login_page.cy.js
+++ cypress/e2e/login_page.cy.js
@@ -2,6 +2,17 @@ describe("The Login Page", () => {
   it("successfully loads", () => {
     cy.visit("/");
 
-    cy.contains("input").click();
+    cy.get('input[name="email"]')
+      .focus()
+      .clear()
+      .type("as6kan.developer@gmail.com");
+    cy.get('input[name="password"]')
+      .focus()
+      .clear()
+      .type("dashboard.ashkan.66");
+
+    cy.get("button").click();
+
+    cy.url().should("include", "/app");
   });
 });
